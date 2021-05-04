f8408e22b10d5eb47baca1c7812d9c892200a225
Nicolas Miller <nicolasmillerr@gmail.com>
2021-05-04 10:21:05 -0300

    refactor

--- main.py
+++ main.py
@@ -1,31 +1,14 @@
 import pandas as pd
-URL = (
-    #"https://www.formula1.com/en/results.html/2020/races/1045/austria/race-result.html"
-    "https://www.formula1.com/en/results.html/2020/races/1045/austria/qualifying.html"
-)
-
 import requests
 from bs4 import BeautifulSoup
 
+URLS = {
+    "results": "https://www.formula1.com/en/results.html/2020/races/1045/austria/race-result.html",
+    "quali": "https://www.formula1.com/en/results.html/2020/races/1045/austria/qualifying.html"
+}
 
-def make_f1_table_for_results(URL):
-    r = requests.get(URL)
-    soup = BeautifulSoup(r.content, "lxml")
-    table = soup.find("table")
-    rows = table.findAll("tr")
-
-    all_rows = []
-    for row in rows[1:]:
-        columns = row.findAll("td")
-        row_as_dict = {}
-        for i, col in enumerate(columns):
-            row_as_dict[i] = col.get_text()
-        all_rows.append(row_as_dict)
-
-
-    df = pd.DataFrame(all_rows)
-    df[3] = df[3].apply(lambda x: x.replace("\n", " "))
-    df = df.rename(columns ={
+# TODO: inferir columnas desde row 1 de tabla html
+RESULTS_COLUMNS = {
         1: "pos",
         2: "#",
         3: "driver",
@@ -33,39 +16,8 @@ def make_f1_table_for_results(URL):
         5: "laps",
         6: "deltat",
         7: "points",
-
-    })
-
-    df= df[["pos",
-    "#",
-    "driver",
-    "team",
-    "laps",
-    "deltat",
-    "points",]]
-
-    return df
-
-
-# TODO: limpiar codigo repetido
-def make_f1_table_for_quali(URL):
-    r = requests.get(URL)
-    soup = BeautifulSoup(r.content, "lxml")
-    table = soup.find("table")
-    rows = table.findAll("tr")
-
-    all_rows = []
-    for row in rows[1:]:
-        columns = row.findAll("td")
-        row_as_dict = {}
-        for i, col in enumerate(columns):
-            row_as_dict[i] = col.get_text()
-        all_rows.append(row_as_dict)
-
-
-    df = pd.DataFrame(all_rows)
-    df[3] = df[3].apply(lambda x: x.replace("\n", " "))
-    df = df.rename(columns ={
+    }
+QUALI_COLUMNS = {
         1: "pos",
         2: "#",
         3: "driver",
@@ -74,20 +26,35 @@ def make_f1_table_for_quali(URL):
         6: "Q2",
         7: "Q3",
         8: "Laps",
+    }
 
-    })
+def get_soup_from_url(URL):
+    r = requests.get(URL)
+    soup = BeautifulSoup(r.content, "lxml")
+    return soup
 
-    df= df[["pos",
-    "#",
-    "driver",
-    "team",
-    "Q1",
-    "Q2",
-    "Q3",
-    "Laps",]]
+def get_table_rows_from_soup(soup):
+    table = soup.find("table")
+    table_rows = table.findAll("tr")
+    all_rows = []
+    for row in table_rows[1:]:
+        columns = row.findAll("td")
+        row_as_dict = {}
+        for i, col in enumerate(columns):
+            row_as_dict[i] = col.get_text()
+        all_rows.append(row_as_dict)
+    return all_rows
+
+def make_datafarme_from_table(table, columns_dict):
+    df = pd.DataFrame(table)
+    df[3] = df[3].apply(lambda x: x.replace("\n", " "))
+
+    df = df.rename(columns = columns_dict)
+    keys = [value for _, value in columns_dict.items()]
+
+    df= df[keys]
 
     return df
-
 
 """
 URLS = ["...."]
